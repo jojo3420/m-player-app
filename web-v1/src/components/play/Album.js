a2a071c2f1f763036ca7a1444f3701f77d9eb41b
jojo3420 <jjjhhhvvv@gmail.com>
2020-09-29 17:18:10 +0900

    file 저장 샘플코드 작성

--- web-v1/src/components/play/Album.js
+++ web-v1/src/components/play/Album.js
@@ -1,7 +1,41 @@
 import React from 'react'
 
-function Album(props) {
-  return <div>Album detail</div>
+function Album({
+  formField,
+  audios,
+  handleField,
+  handleAudios,
+  onSubmitFileSave,
+}) {
+  const { title, artist, genre } = formField
+  console.log({ title, artist, genre })
+  return (
+    <>
+      <h2>Album detail</h2>
+      <form onSubmit={onSubmitFileSave}>
+        <label>
+          파일명: <input value={title} name="title" onChange={handleField} />
+        </label>
+        {/*<label>*/}
+        {/*  아티스트:*/}
+        {/*  <input value={artist} name={'artist'} onChange={handleField} />*/}
+        {/*</label>*/}
+        {/*<label>*/}
+        {/*  장르: <input value={genre} name={'genre'} onChange={handleField} />*/}
+        {/*</label>*/}
+        <label>
+          오디오 파일:
+          <input
+            type="file"
+            name="audios"
+            multiple={true}
+            onChange={handleAudios}
+          />
+        </label>
+        <button type={'submit'}>오디오 저장</button>
+      </form>
+    </>
+  )
 }
 
 export default Album
--- web-v1/src/containers/paly/AlbumContainer.js
+++ web-v1/src/containers/paly/AlbumContainer.js
@@ -1,6 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import Album from 'components/play/Album'
+import axios from 'axios'
+import { message } from 'antd'
 
-export default function AlbumContainer(props) {
-  return <Album />
+export default function AlbumContainer() {
+  const [formField, setField] = useState({
+    title: '',
+    artist: '',
+    genre: '',
+  })
+  const [audios, setAudios] = useState([])
+  const handleField = useCallback((e) => {
+    const { name, value } = e && e.target
+    console.log({ name, value })
+    setField((item) => ({ ...item, [name]: value }))
+  }, [])
+  const handleAudios = useCallback((e) => {
+    const { files } = e && e.target
+    // console.log({ files })
+    setAudios(files)
+  }, [])
+
+  const onSubmitFileSave = useCallback(
+    async (e) => {
+      e.preventDefault()
+      const formData = new FormData()
+      formData.append('file', audios)
+      try {
+        const response = await axios.post('/v1/upload', formData)
+        message.success(response)
+      } catch (error) {
+        console.log({ error })
+        message.error('upload 실패..')
+      }
+    },
+    [audios],
+  )
+
+  return (
+    <Album
+      formField={formField}
+      audios={audios}
+      handleField={handleField}
+      handleAudios={handleAudios}
+      onSubmitFileSave={onSubmitFileSave}
+    />
+  )
 }
